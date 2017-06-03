57582a1173ed8ddd403ea4e7260db7c868b8725b
Sumedh Jigjinni <sjigjinni@gmail.com>
2017-06-03 16:49:34 -0700

    Setting up devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Required for Devise/Heroku
+  config.action_mailer.default_url_options = { host: 'sumedh-finteresting.herokuapp.com'}
 end
